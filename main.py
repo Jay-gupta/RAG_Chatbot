7a00af044888fda445b5737b3bd90f7567dace38
Jay Gupta <cooljoy.gupta@gmail.com>
2025-02-28 09:59:25 +0000

    nltk fix

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
+import nltk
+nltk.data.path.append(os.path.join(os.path.dirname(__file__), "nltk_data"))
+
 import os
 import json
 
